19b7382801694fee96acb6fff0f8dcdee03dbc79
Joseph Kirkup <164064098+jlk31@users.noreply.github.com>
2025-04-09 12:23:25 +0000

    Added relational db

--- database/create_db.py
+++ database/create_db.py
@@ -7,17 +7,36 @@ import sqlite3
 connection = sqlite3.connect('user_db.db')
 cursor = connection.cursor()
 
+#users table creation
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS users (
                username TEXT PRIMARY KEY,
                password TEXT NOT NULL,
 )''')
 
+#high_scores table creation
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS high_scores (
+               user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+               username TEXT NOT NULL,
+               score INTEGER NOT NULL,
+               FOREIGN KEY (username) REFERENCES users (username)
+)''')
+
+#sample data 
 cursor.execute('''
 INSERT INTO users (username, password) VALUES
 ('user1', 'password1')
 ('user2', 'password2')
 ('user3', 'password3')
+)''')
+
+#sample data
+cursor.execute('''
+INSERT INTO high_scores (username, score) VALUES
+('user1', 100),
+('user2', 200),
+('user3', 300)
 )''')
 
 connection.commit()
