9125167171140c5dc2ef7880a212c1fd955e035d
jlk31 <joe.kirk31'hotmail.com>
2025-04-30 12:27:31 +0100

    Changes made to button

--- src/utils/button.py
+++ src/utils/button.py
@@ -10,9 +10,9 @@ import pygame
 
 class Button():
 	def __init__(self,x, y, image, scale):
-		width = image.get_width()
-		height = image.get_height()
-		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+		WIDTH = image.get_width()
+		HEIGHT = image.get_height()
+		self.image = pygame.transform.scale(image, (int(WIDTH * scale), int(HEIGHT * scale)))
 		self.rect = self.image.get_rect()
 		self.rect.topleft = (x, y)
 		self.clicked = False
